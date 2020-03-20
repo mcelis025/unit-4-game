9ec2cdaceab472f6c44e03c9bd9278367c5703c8
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-20 01:06:36 -0600

    added characters as objects // psuedo code added

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+//Variables
+
+//Character  Objects
+var saitama = {
+  name: "Saitama",
+  health: 500,
+  attack: 500
+}
+
+var genos = {
+  name: "Genos",
+  health: 500,
+  attack: 500
+}
+
+var boros = {
+  name: "Lord Boros",
+  health: 500,
+  attack: 500
+}
+
+var crablante = {
+  name: "Crablante",
+  health: 500,
+  attack: 500
+}
+
+//Pick Character
+
+  //When character chosen move to your character
+  //Move enemy characters to enemies available to attack
+
+//Pick Enemy from enemies available to attack
+
+  //Move chosen character to Defender slot
+
+//When Attack Btn is clicked
+  
+  //Show how much damage done to enemy
+  //Show how much damage taken
+  //Update health for both characters
+
+  //if health = 0 
+    //I lose
+  //if enemy health = 0
+    //enemy dies
+    //if enemy dies then choose new enemy
+    //if enemies = 0 
+      //then game is won 
+      //restart game btn
