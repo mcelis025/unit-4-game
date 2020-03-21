3bfeb6bd7a1e0ff3c8b0228ec97c954cc2860c51
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-20 22:30:19 -0600

    added more variables needed // made the selected character go to your character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,68 @@
 //Variables
+var mainCharacter;
+var enemyOne;
+var enemyTwo;
+var enemyThree;
+var enemySelected;
 
 //Character  Objects
 var saitama = {
   name: "Saitama",
-  health: 500,
-  attack: 500
+  healthPoints: 500,
+  attackPower: 50,
+  counterAttackPower: 20
 }
 
 var genos = {
   name: "Genos",
-  health: 500,
-  attack: 500
+  healthPoints: 150,
+  attackPower: 5,
+  counterAttackPower: 20
 }
 
 var boros = {
   name: "Lord Boros",
-  health: 500,
-  attack: 500
+  healthPoints: 200,
+  attackPower: 8,
+  counterAttackPower: 20
 }
 
 var crablante = {
   name: "Crablante",
-  health: 500,
-  attack: 500
+  healthPoints: 50,
+  attackPower: 2,
+  counterAttackPower: 20
 }
 
 //Pick Character
+$("#saitamaClick").click(function() {
+  mainCharacter = $('<div class="container p-0 bg-secondary pt-4" id="saitamaClick"> <img src="assets/images/saitama.jpg" alt="saitama" class="mx-auto d-block saitamaImg"/> <p class="text-center myText">Saitama</p> <p class="text-center myText">Health: </p> </div>');
+  $("#saitamaClick").remove().addClass("saitama");
+  $("#genosClick").remove();
+  $("#borosClick").remove();
+  $("#crablanteClick").remove();
+
+  $("#myHero").append(mainCharacter);
+    mainCharacter.attr('id', 'saitamaClick');
+  $("#enemy1").append(mainCharacter);
+    mainCharacter.attr('id', 'genosClick');
+  $("#enemy2").append(mainCharacter);
+    mainCharacter.attr('id', 'borosClick');
+  $("#enemy3").append(mainCharacter);
+    mainCharacter.attr('id', 'crablanteClick');
+});
+
+$("#genosClick").click(function() {
+
+});
+
+$("#borosClick").click(function() {
+
+});
+
+$("#crablanteClick").click(function() {
+
+});
 
   //When character chosen move to your character
   //Move enemy characters to enemies available to attack
