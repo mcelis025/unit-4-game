3877eaec39970011fea44316a9b70bae12a7d59f
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-03-28 18:13:11 -0600

    added characters object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,3 +9,33 @@ $(document).ready(function () {
   var restart = false;
   var resetBtn = true;
   var endGame = false;
+
+  //Character  Objects
+  var characters = [
+    {
+      name: "Saitama",
+      healthPoints: 500,
+      attackPower: 50,
+      counterAttackPower: 20
+    },
+
+    {
+      name: "Genos",
+      healthPoints: 150,
+      attackPower: 5,
+      counterAttackPower: 20
+    },
+
+    {
+      name: "Boros",
+      healthPoints: 200,
+      attackPower: 8,
+      counterAttackPower: 20
+    },
+
+    {
+      name: "Crablante",
+      healthPoints: 50,
+      attackPower: 2,
+      counterAttackPower: 20
+    }];
